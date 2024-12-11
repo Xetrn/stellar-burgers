9975e476396ae19ee19a9376423db01db305c5e2
irkatyman <tymanira@mail.ru>
2024-12-11 17:56:35 +0500

    feat: update

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,11 @@
 import { FC } from 'react';
+
 import { AppHeaderUI } from '@ui';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+import { useSelector } from '../../services/store';
+import { getName } from '../../services/slices/user';
+
+export const AppHeader: FC = () => {
+  const userName = useSelector(getName);
+  return <AppHeaderUI userName={userName} />;
+};
