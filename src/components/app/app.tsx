75872539d2847975fce72baeef0dd1443c50b6ff
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-12-12 23:24:10 +0500

    refactor: routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,17 +12,13 @@ import {
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { AppHeader } from '@components';
 import { useEffect } from 'react';
 import { useDispatch } from '../../services/store';
 import { fetchIngredients, fetchUser } from '@slices';
-import { ProtectedRoute } from '../protected-route';
+import { AppRoutes, ModalRoutes } from './router';
 
 const App = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const backgroundLocation = location.state?.background;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -33,59 +29,8 @@ const App = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Routes location={backgroundLocation || location}>
-        <Route path='/' element={<ConstructorPage />} />
-        <Route path='/feed' element={<Feed />} />
-        <Route path='/feed/:number' element={<OrderInfo />} />
-        <Route path='/ingredients/:id' element={<IngredientDetails />} />
-
-        <Route element={<ProtectedRoute notAuth />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-        </Route>
-
-        <Route element={<ProtectedRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/profile/orders' element={<ProfileOrders />} />
-          <Route path='/profile/orders/:number' element={<OrderInfo />} />
-        </Route>
-
-        <Route path='*' element={<NotFound404 />} />
-      </Routes>
-
-      {backgroundLocation && (
-        <Routes>
-          <Route
-            path='/ingredients/:id'
-            element={
-              <Modal title={'ewewe'} onClose={() => navigate(-1)}>
-                <IngredientDetails />
-              </Modal>
-            }
-          />
-          <Route
-            path='/feed/:number'
-            element={
-              <Modal title={''} onClose={() => navigate(-1)}>
-                <OrderInfo />
-              </Modal>
-            }
-          />
-
-          <Route element={<ProtectedRoute />}>
-            <Route
-              path='/profile/orders/:number'
-              element={
-                <Modal title={''} onClose={() => navigate(-1)}>
-                  <OrderInfo />
-                </Modal>
-              }
-            />
-          </Route>
-        </Routes>
-      )}
+      <AppRoutes />
+      <ModalRoutes />
     </div>
   );
 };
---
+++ src/components/app/router/modal-routes.tsx
@@ -0,0 +1,49 @@
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Modal,
+  IngredientDetails,
+  OrderInfo,
+  ProtectedRoute
+} from '@components';
+
+export const ModalRoutes = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
+
+  return (
+    <>
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title={'ewewe'} onClose={() => navigate(-1)}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title={''} onClose={() => navigate(-1)}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+
+          <Route element={<ProtectedRoute />}>
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <Modal title={''} onClose={() => navigate(-1)}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Route>
+        </Routes>
+      )}
+    </>
+  );
+};
