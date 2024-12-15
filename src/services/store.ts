48daf37fa5187cc1ebc4c5e04cc4cfc867392657
kirpav30 <kirpav30@gmail.com>
2024-12-16 00:13:00 +0500

    Роутинг и авторизация

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,19 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { IngredientsSlice } from './ingredientsSlice';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { BurgerConstructorSlice } from './burgerConstructorSlice';
+import { UserSlice } from './userSlice';
+import { OrderSlice } from './orderSlice';
+import { FeedSlice } from './feedSlice';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = {
+  [IngredientsSlice.name]: IngredientsSlice.reducer,
+  [BurgerConstructorSlice.name]: BurgerConstructorSlice.reducer,
+  [UserSlice.name]: UserSlice.reducer,
+  [OrderSlice.name]: OrderSlice.reducer,
+  [FeedSlice.name]: FeedSlice.reducer
+};
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
